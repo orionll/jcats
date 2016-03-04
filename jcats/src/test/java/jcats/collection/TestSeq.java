ce20d38a9f37d07e367e326e21479c625556d661
orionll <orionllmain@gmail.com>
2016-03-04 20:02:54 +0600

    Moved TestSeq to jcats.collection package

--- jcats/src/test/java/jcats/collection/TestSeq.java
+++ jcats/src/test/java/jcats/collection/TestSeq.java
@@ -1,6 +1,7 @@
+package jcats.collection;
+
 import static jcats.collection.Seq.emptySeq;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import jcats.collection.Seq;
 import org.junit.Assert;
